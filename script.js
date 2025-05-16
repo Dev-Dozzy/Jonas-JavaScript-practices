09425642087bd78e532856ad3c9bbc9c5a4de925
Dev-Dozzy <chukwuma.chiedoziepre@gmail.com>
2025-05-16 14:08:17 +0100

    First commit on my personal practices on js

---
+++ script.js
@@ -0,0 +1,47 @@
+/*
+var firstName='Chiedozie';
+console.log(firstName);
+
+var lastName = 'Chukwuma';
+var age = 26;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+/*Variable mutation */
+
+/*Type coerction*/ 
+var firstName = 'John';
+var age = 28;
+console.log(firstName + " " + '28')
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+/*Variable Mutation*/
+age = 'twenty eight';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
