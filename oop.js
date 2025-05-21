05d8c889357508fd3f9095149255b214b67b530b
Dev-Dozzy <chukwuma.chiedoziepre@gmail.com>
2025-05-21 12:22:30 +0100

    starting learning Object oriented programming

---
+++ oop.js
@@ -0,0 +1,59 @@
+/*
+class Car {
+  constructor(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  drive() {
+    return `Driving a ${this.year} ${this.brand} ${this.model}`;
+  }
+
+  stop() {
+    return `Stopping the ${this.year} ${this.brand} ${this.model}`;
+  }
+}
+
+const honda = new Car("Toyota", "Corolla", 2008);
+const drive = honda.drive();
+const stop = honda.stop();
+
+console.log(drive);
+console.log(stop);
+*/
+
+
+/*
+class Bicycle {
+  constructor(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  ride(){
+    return `John is riding his ${this.year} ${this.model} ${this.brand} bicycle.`
+  }
+}
+
+const Bmx = new Bicycle("Mercedes", "BMX", 2005);
+const ride = Bmx.ride();
+
+console.log(ride);
+*/
+
+class Chair {
+  constructor(brand, color, year){
+    this.brand = brand;
+    this.color = color;
+    this.year = year;
+  }
+
+  sit(){
+    return `The CEO has a ${this.color} ${this.year} ${this.brand} chair in his office.`
+  }
+}
+
+const ceo = new Chair("furgle", "white", "2025");
+console.log(ceo.sit());
